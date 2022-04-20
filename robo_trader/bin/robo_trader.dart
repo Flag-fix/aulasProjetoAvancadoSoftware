f9ca61bbcb34abf0a92a24757cf1225c1839eb9c
Flag-fix <isac.bandeir4@gmail.com>
2022-04-20 20:38:20 -0300

    Envio aula 20/04/2022

---
+++ robo_trader/bin/robo_trader.dart
@@ -0,0 +1,5 @@
+import 'package:robo_trader/robo_trader.dart' as robo_trader;
+
+void main(List<String> arguments) {
+  print('Hello world: ${robo_trader.calculate()}!');
+}
---
+++ robo_trader/test/robo_trader_test.dart
@@ -0,0 +1,8 @@
+import 'package:robo_trader/robo_trader.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
